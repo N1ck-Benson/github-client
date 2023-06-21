82ae3a967629807233c7246b68c00c6b1f4ace24
Nick Benson <nick.benson@digitalworkroom.co.uk>
2023-06-21 18:23:58 +0100

    feat: add items to list, basic ui

--- src/components/ListView/ListView.tsx
+++ src/components/ListView/ListView.tsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./ListView.scss";
+import { Ctx } from "../CtxProvider";
 
 function ListView() {
+  const { list } = useContext(Ctx);
+
   return (
-    <div className="ListView">
+    <div className={`ListView ${!list || list.length === 0 ? "empty" : ""}`}>
       <table>
-        <tr>repo 1</tr>
-        <tr>repo 2</tr>
-        <tr>...</tr>
+        {list?.map((item, i) => (
+          <tr key={`list_item-${i}`}>
+            <a href={item.html_url}>link</a>
+            <p>{item.name}</p>
+            <p>{item.owner.login}</p>
+          </tr>
+        ))}
       </table>
     </div>
   );
