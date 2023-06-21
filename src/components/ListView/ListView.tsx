5a7f607d749d819d8afe2850368505e6cc3fef5d
Nick Benson <nick.benson@digitalworkroom.co.uk>
2023-06-21 18:59:19 +0100

    feat: add pagination to ListView

--- src/components/ListView/ListView.tsx
+++ src/components/ListView/ListView.tsx
@@ -1,21 +1,37 @@
 import React, { useContext } from "react";
 import "./ListView.scss";
 import { Ctx } from "../CtxProvider";
+import { setData } from "../../api";
 
 function ListView() {
-  const { list } = useContext(Ctx);
+  const { list, setList, page, term } = useContext(Ctx);
+  const handlePage = (e: React.SyntheticEvent) => {
+    const target = e.target as HTMLButtonElement;
+    const { id } = target;
+    let nextPage = id === "forward" ? page + 1 : id === "back" ? page - 1 : +id;
+    setData([term, nextPage], setList);
+  };
 
   return (
     <div className={`ListView ${!list || list.length === 0 ? "empty" : ""}`}>
-      <table>
-        {list?.map((item, i) => (
-          <tr key={`list_item-${i}`}>
-            <a href={item.html_url}>link</a>
-            <p>{item.name}</p>
-            <p>{item.owner.login}</p>
-          </tr>
+      <div className="inner">
+        <table>
+          {list?.map((item, i) => (
+            <tr key={`list_item-${i}`}>
+              <a href={item.html_url}>link</a>
+              <p>{item.name}</p>
+              <p>{item.owner.login}</p>
+            </tr>
+          ))}
+        </table>
+      </div>
+      <div className="pagination">
+        {["back", page - 1, page, page + 1, "forward"].map((action) => (
+          <button id={action.toString()} onClick={handlePage}>
+            {action}
+          </button>
         ))}
-      </table>
+      </div>
     </div>
   );
 }
